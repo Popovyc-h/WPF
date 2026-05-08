6138125048994f1c5abc330141f829b1859b9ca3
Popovyc-h <popovichtanja86@gmail.com>
2026-05-08 12:53:44 +0300

    Update MainWindow.xaml.cs

--- hw3/MainWindow.xaml.cs
+++ hw3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace TextAnalyzerApp;
@@ -8,6 +9,8 @@ namespace TextAnalyzerApp;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private CancellationTokenSource _cancellationTokenSource;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -17,7 +20,7 @@ public partial class MainWindow : Window
         string text = InputTextBox.Text;
         var analyzer = new TextAnalyzer();
         await Task.Run(() => analyzer.Analyze(text));
-        ResultTextBlock.Text = analyzer.ToString();
+        ResultTextBlock.Text = BuildReport(analyzer);
     }
 
     private async void DownloadInFileButton_Click(object sender, RoutedEventArgs e)
@@ -25,6 +28,44 @@ public partial class MainWindow : Window
         string text = InputTextBox.Text;
         var analyzer = new TextAnalyzer();
         await Task.Run(() => analyzer.Analyze(text));
-        await File.WriteAllTextAsync("report.txt", analyzer.ToString());
+        await File.WriteAllTextAsync("report.txt", BuildReport(analyzer));
+    }
+
+    private void StopButton_Click(object sender, RoutedEventArgs e)
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    private async void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        _cancellationTokenSource = new CancellationTokenSource();
+        var token = _cancellationTokenSource.Token;
+
+        string text = InputTextBox.Text;
+        var analyzer = new TextAnalyzer();
+        await Task.Run(() => analyzer.Analyze(text), token);
+        ResultTextBlock.Text = BuildReport(analyzer);
+    }
+
+    private string BuildReport(TextAnalyzer analyzer)
+    {
+        var result = new StringBuilder();
+
+        if (SentenceCountBox.IsChecked == true)
+            result.AppendLine($"Кількість речень: {analyzer.sentenceCount}");
+
+        if (CharCountBox.IsChecked == true)
+            result.AppendLine($"Кількість символів: {analyzer.charCount}");
+
+        if (WordCountBox.IsChecked == true)
+            result.AppendLine($"Кількість слів: {analyzer.wordCount}");
+
+        if (QuestionCountBox.IsChecked == true)
+            result.AppendLine($"Кількість питальних речень: {analyzer.questionCount}");
+
+        if (ExclamationCountBox.IsChecked == true)
+            result.AppendLine($"Кількість окличних речень: {analyzer.exclamationCount}");
+
+        return result.ToString();
     }
 }
